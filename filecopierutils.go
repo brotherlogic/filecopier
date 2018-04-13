393f096c38c6e691907c18901c5a91e768f84266
Simon Tucker <brotherlogic@gmail.com>
2018-04-13 15:48:58 -0700

    Adds ability to read and write authorized keys.

--- filecopierutils.go
+++ filecopierutils.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func makeCopyString(server, file string) string {
 	if len(server) == 0 {
@@ -8,4 +13,41 @@ func makeCopyString(server, file string) string {
 	}
 
 	return fmt.Sprintf("%v@%v", server, file)
+}
+
+func readKeys(filename string) (map[string]string, error) {
+	keys := make(map[string]string)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return keys, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		pieces := strings.Fields(scanner.Text())
+		bits := strings.Split(pieces[2], "@")
+		keys[bits[1]] = pieces[1]
+	}
+
+	return keys, nil
+}
+
+func writeKeys(file string, keys map[string]string) error {
+	f, err := os.Create(file)
+	defer f.Close()
+
+	if err != nil {
+		return err
+	}
+
+	w := bufio.NewWriter(f)
+	for key, value := range keys {
+		w.WriteString(fmt.Sprintf("ssh-rsa %v simon@%v\n", value, key))
+	}
+	w.Flush()
+
+	return nil
+
 }
--- filecopierutils_test.go
+++ filecopierutils_test.go
@@ -8,3 +8,55 @@ func TestServerExpression(t *testing.T) {
 		t.Errorf("Bad copy string: %v", str)
 	}
 }
+func TestBadRead(t *testing.T) {
+	_, err := readKeys("madeup/blah.txt")
+	if err == nil {
+		t.Errorf("Bad read did not fail: %v")
+	}
+}
+
+func TestBadWrite(t *testing.T) {
+	err := writeKeys("madeup/blah.txt", make(map[string]string))
+	if err == nil {
+		t.Errorf("Bad write did not fail: %v")
+	}
+}
+
+func TestReadKeys(t *testing.T) {
+	keys, err := readKeys("testdata/testf.txt")
+	if err != nil {
+		t.Fatalf("Unable to load keys: %v", err)
+	}
+	val, ok := keys["tasklist"]
+	if !ok {
+		t.Fatalf("Unable to find key for tasklist: %v", keys)
+	}
+
+	if val != "AAAAB3NzaC1yc2EAAAADAQABAAABAQC0ME/rBV/P73sMwapKxQh4hVujgSK8XeWpyLLwSliEnrLmkGREViFMaTGMFkcRdmOPdaxsFe0QWQF+7HshorMexGewfNP/g9+jy433slBF4GkQtvrTMNhi2rQATyIo/Efvhb5QRPSmV5TaC8xjxi/h5JB4OpvNMQ9HKtGj34mohNftCwfai46P0s8t3TbUgSIpXAAwi8bQwEENuNl9DlllCpMQT8ZIcux5DITk7LR74/FoQaugn30oI7EbtlJu5DXYqUfQuX6t2WFTpoIEcgBxSAz97jOUEPP4JkEQ8MFWpp8ibumma+p0mR9ooAiwHkZF9+qLolcMn0hHsB8eXkIr" {
+		t.Errorf("Keys do not match: %v", val)
+	}
+}
+
+func TestWriteKeys(t *testing.T) {
+	keys, err := readKeys("testdata/testf.txt")
+	if err != nil {
+		t.Fatalf("Unable to load keys: %v", err)
+	}
+	err = writeKeys("testdata/testg.txt", keys)
+	if err != nil {
+		t.Fatalf("Unable to write keys: %v", err)
+	}
+
+	keys, err = readKeys("testdata/testg.txt")
+	if err != nil {
+		t.Fatalf("Unable to read keys: %v", err)
+	}
+	val, ok := keys["tasklist"]
+	if !ok {
+		t.Fatalf("Unable to find key for tasklist: %v", keys)
+	}
+
+	if val != "AAAAB3NzaC1yc2EAAAADAQABAAABAQC0ME/rBV/P73sMwapKxQh4hVujgSK8XeWpyLLwSliEnrLmkGREViFMaTGMFkcRdmOPdaxsFe0QWQF+7HshorMexGewfNP/g9+jy433slBF4GkQtvrTMNhi2rQATyIo/Efvhb5QRPSmV5TaC8xjxi/h5JB4OpvNMQ9HKtGj34mohNftCwfai46P0s8t3TbUgSIpXAAwi8bQwEENuNl9DlllCpMQT8ZIcux5DITk7LR74/FoQaugn30oI7EbtlJu5DXYqUfQuX6t2WFTpoIEcgBxSAz97jOUEPP4JkEQ8MFWpp8ibumma+p0mR9ooAiwHkZF9+qLolcMn0hHsB8eXkIr" {
+		t.Errorf("Keys do not match: %v", val)
+	}
+}
