88d6c968fbb37f52b913f2fc7343d627126d95e0
Simon Tucker <brotherlogic@gmail.com>
2018-09-27 18:38:10 -0700

    Be verbose in copy info

--- filecopierapi.go
+++ filecopierapi.go
@@ -50,7 +50,7 @@ func (s *Server) Copy(ctx context.Context, in *pb.CopyRequest) (*pb.CopyResponse
 
 	copyIn := makeCopyString(in.InputServer, in.InputFile)
 	copyOut := makeCopyString(in.OutputServer, in.OutputFile)
-	command := exec.Command(s.command, "-3", "-o", "StrictHostKeyChecking=no", copyIn, copyOut)
+	command := exec.Command(s.command, "-v", "-3", "-o", "StrictHostKeyChecking=no", copyIn, copyOut)
 
 	output := ""
 	out, err := command.StderrPipe()
