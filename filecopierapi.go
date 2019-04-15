8f2541e4fa40ad527c9598c5cfa7b1e901bc3f61
Simon Tucker <simon@brotherlogic@gmail>
2019-04-15 10:37:21 -0700

    Better logging of copy slowness

--- filecopierapi.go
+++ filecopierapi.go
@@ -125,6 +125,12 @@ func (s *Server) runCopy(ctx context.Context, in *pb.CopyRequest) error {
 	}
 
 	s.copyTime = time.Now().Sub(stTime)
+	s.tCopyTime += time.Now().Sub(stTime)
+
+	if s.copyTime > time.Minute*5 {
+		s.RaiseIssue(ctx, "Long Copy Time", fmt.Sprintf("Copy took %v", s.copyTime), false)
+	}
+
 	s.lastError = fmt.Sprintf("DONE %v", output)
 	return nil
 }
