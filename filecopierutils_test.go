da9a977029068341e3834e7cd16ed2315a3d5246
Simon Tucker <simon@brotherlogic@gmail>
2019-04-15 08:48:29 -0700

    Adds test of running empty queue

--- filecopierutils_test.go
+++ filecopierutils_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"golang.org/x/net/context"
+)
 
 func TestServerExpression(t *testing.T) {
 	str := makeCopyString("server", "file")
@@ -58,5 +62,13 @@ func TestWriteKeys(t *testing.T) {
 
 	if val != "AAAAB3NzaC1yc2EAAAADAQABAAABAQC0ME/rBV/P73sMwapKxQh4hVujgSK8XeWpyLLwSliEnrLmkGREViFMaTGMFkcRdmOPdaxsFe0QWQF+7HshorMexGewfNP/g9+jy433slBF4GkQtvrTMNhi2rQATyIo/Efvhb5QRPSmV5TaC8xjxi/h5JB4OpvNMQ9HKtGj34mohNftCwfai46P0s8t3TbUgSIpXAAwi8bQwEENuNl9DlllCpMQT8ZIcux5DITk7LR74/FoQaugn30oI7EbtlJu5DXYqUfQuX6t2WFTpoIEcgBxSAz97jOUEPP4JkEQ8MFWpp8ibumma+p0mR9ooAiwHkZF9+qLolcMn0hHsB8eXkIr" {
 		t.Errorf("Keys do not match: %v", val)
+	}
+}
+
+func TestRunEmptyQueue(t *testing.T) {
+	s := InitTestServer()
+	err := s.runQueue(context.Background())
+	if err != nil {
+		t.Errorf("Error running queue: %v", err)
 	}
 }
