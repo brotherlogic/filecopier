6718e6d48c297a80a1faeb1495a695f4eb2a4ad0
Simon Tucker <simon@brotherlogic@gmail>
2019-11-16 12:23:53 -0800

    File copier has no master

--- filecopier.go
+++ filecopier.go
@@ -327,6 +327,7 @@ func main() {
 
 	server.keySetup()
 	err := server.RegisterServer("filecopier", false)
+	server.Registry.IgnoresMaster = true
 
 	if err == nil {
 		server.RegisterRepeatingTaskNonMaster(server.shareKeys, "share_keys", time.Hour)
