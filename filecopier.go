e42c88d952cec856e7ad4d29c163996318ab2ef5
Simon Tucker <brotherlogic@gmail.com>
2022-03-30 13:00:06 -0700

    Huh. This closes #810

--- filecopier.go
+++ filecopier.go
@@ -276,7 +276,7 @@ func (s *Server) runCopy(in *pb.CopyRequest) error {
 		} else if strings.Contains("differs", output) {
 			s.RaiseIssue("Found mismatch error", fmt.Sprintf("[%v] Error on copy: %v (%v -> %v)", s.Registry.Identifier, output, copyIn, copyOut))
 		} else {
-			s.RaiseIssue("Copy Error", fmt.Sprintf("[%v] Error on copy: %v (%v -> %v)", s.Registry.Identifier, output, copyIn, copyOut))
+			s.RaiseIssue("Copy Error", fmt.Sprintf("[%v] Error on the copy: %v (%v -> %v)", s.Registry.Identifier, output, copyIn, copyOut))
 		}
 	}
 
